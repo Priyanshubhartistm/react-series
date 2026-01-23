fbca9db295d1c0e63e0f44284eb8a14fd5798675
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-01-23 22:08:39 +0530

    react basic

---
+++ usestate/react/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Counter from './components/Counter.jsx'
+
+
+import React from 'react'
+
+
+function App() {
+
+
+  return (
+
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+export default App

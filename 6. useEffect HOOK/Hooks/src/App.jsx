7b7af333b53f0964a58a90621d36d55383f262d9
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-01-29 23:45:21 +0530

    useeffect

--- 6. useEffect HOOK/Hooks/src/App.jsx
+++ 6. useEffect HOOK/Hooks/src/App.jsx
@@ -1,21 +1,61 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
 
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(1);
 
+  // first = side effect function
+  // second = cleanup function
+  // third = dependency array, comma separated dep list
+
+  // variation:1
+  // runs on evey render
+  // useEffect(() => {
+  //   alert("I wil never run on each render")
+  // })
+
+  // variation:2
+  // that runs on only first render
+  // useEffect(() => {
+  //   alert("I will run only once on first render")
+  // }, [])
+
+  // variation:3
+  // runs on first render and when count changes
+  // useEffect(() => {
+  //   alert(`Count has changed to ${count}`);
+  // }, [count])
+
+  // variation:4
+  // runs on first render and when count changes
+  // with cleanup function
   useEffect(() => {
-    console.log('Component mounted')
+    alert(`Count has changed to ${count}`);
 
-    return () => {
-      console.log('Component will unmount')
-    }
-  }, [])
-   
+  }, [count])
+
+  function handleClick() {
+    setCount(count+1);
+    setTotal(total+1);
+  }
+  function handleClickTotal() {
+  }
+
 
   return (
     <div>
-      hello
+      <button onClick={handleClick}>update count</button>
+      <br/>
+      Count is {count}
+
+
+      <br/>
+      <button onClick={handleClickTotal}>update count</button>
+      <br/>
+      Total is {total}
+      
     </div>
   )
 }
